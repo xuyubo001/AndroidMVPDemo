4fe739c3a2d12a64ffe3f50a2b509e91af3872f9
xuyubo001 <xuyubo001@163.com>
2017-12-25 11:34:01 +0800

    Initial commit

---
+++ app/src/main/java/com/nsu/edu/androidmvpdemo/login/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.nsu.edu.androidmvpdemo.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.nsu.edu.androidmvpdemo.R;
+
+/**
+ * Created by Anthony on 2016/2/15.
+ * Class Note:MVP模式中View层对应一个activity，这里是登陆的activity
+ * demo的代码流程：Activity做了一些UI初始化的东西并需要实例化对应
+ * LoginPresenter的引用和实现 LoginView的接口，监听界面动作，
+ * Go按钮按下后即接收到查询天气的事件，在onClick里接收到即通过LoginPresenter
+ * 的引用把它交给LoginPresenter处理。LoginPresenter接收到了登陆的逻辑就知道要登陆了，
+ * 然后LoginPresenter显示进度条并且把逻辑交给我们的Model去处理，也就是这里面的LoginModel，
+ * （LoginModel的实现类LoginModelImpl），同时会把OnLoginFinishedListener也就是LoginPresenter
+ * 自身传递给我们的Model（LoginModel）。LoginModel处理完逻辑之后，结果通过
+ * OnLoginFinishedListener回调通知LoginPresenter，LoginPresenter再把结果返回给view层的Activity，
+ * 最后activity显示结果
+ */
+public class LoginActivity extends Activity implements LoginView, View.OnClickListener {
+
+    private ProgressBar progressBar;
+    private EditText username;
+    private EditText password;
+    private LoginPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        findViewById(R.id.button).setOnClickListener(this);
+
+        presenter = new LoginPresenterImpl(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void setUsernameError() {
+        username.setError(getString(R.string.username_error));
+    }
+
+    @Override
+    public void setPasswordError() {
+        password.setError(getString(R.string.password_error));
+    }
+
+    @Override
+    public void navigateToHome() {
+// TODO       startActivity(new Intent(this, MainActivity.class));
+        Toast.makeText(this,"login success",Toast.LENGTH_SHORT).show();
+//        finish();
+        hideProgress();
+    }
+
+    @Override
+    public void onClick(View v) {
+        presenter.validateCredentials(username.getText().toString(), password.getText().toString());
+    }
+
+}

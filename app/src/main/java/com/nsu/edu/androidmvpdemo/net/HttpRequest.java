2be0646ca53d3393e1ef818b07ba0589208e808e
xuyubo001 <xuyubo001@163.com>
2017-12-25 17:59:28 +0800

    add netmanager

---
+++ app/src/main/java/com/nsu/edu/androidmvpdemo/net/HttpRequest.java
@@ -0,0 +1,92 @@
+package com.nsu.edu.androidmvpdemo.net;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.nsu.edu.androidmvpdemo.net.net.IHttpCallback;
+import com.nsu.edu.androidmvpdemo.net.net.NetManager;
+import com.nsu.edu.androidmvpdemo.net.tools.Logs;
+import com.nsu.edu.androidmvpdemo.utils.UrlPath;
+
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * Created by Administrator on 2015/10/19 0019.
+ */
+public class HttpRequest {
+	public static final String TAG = "HttpRequest";
+	public static final int DONE = 9993;
+	public static final int UPDATE = 9994;
+	public static final int UPLOAD_FAIL = 9996;
+	public static final int UPLOAD_SUCCESS = 9995;
+	public static final int UPLOAD_SUCCESS_QUESTION = 9994;
+	public static final int INITDATA = 9997;
+	public static final int SUCCESS = 9998;
+	public static final int ERROR = 9999;
+	public static final int UPDATE_RESULTID = 9992;
+	private static List<String> waitDownLoadId;
+	private static int nowDownLoadNo = 0;
+
+
+	/**
+	 * 传给服务器客户端的CID 用于推送
+	 *
+	 * @param context
+	 *            上下文
+	 */
+	public static void updateClientId(final Context context,String teacherId) {
+		NetManager netManager = new NetManager();
+		List<NameValuePair> params = new ArrayList<>();
+
+		params.add(new BasicNameValuePair("teacherId",teacherId));
+
+		netManager.httpPost(UrlPath.UPDATE_CLIENT_ID(), null, params, new IHttpCallback() {
+			@Override
+			public void onStart() {
+
+
+			}
+
+			@Override
+			public void onSuccess(String msg) {
+
+
+			}
+
+			@Override
+			public void onFailure(String err) {
+
+
+			}
+
+			@Override
+			public void onProgress(long progress, long maxValue) {
+
+			}
+		});
+	}
+
+
+
+
+	
+
+
+
+
+
+
+}
